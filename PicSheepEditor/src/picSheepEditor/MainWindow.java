875a3e1b61b578e40c1c3f49a07d7e7e00d8eafa
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2021-11-15 20:56:08 +0100

    Add files via upload
    draw rectangle and line

--- PicSheepEditor/src/picSheepEditor/MainWindow.java
+++ PicSheepEditor/src/picSheepEditor/MainWindow.java
@@ -3,10 +3,13 @@ package picSheepEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
@@ -17,12 +20,14 @@ public class MainWindow {
 int X,Y;
 	Image image;
 	Boolean mouse=false;
+	Boolean bleistift = false;
+	Boolean viereck = false;
  public void displayIt(){
 	  final Display display = new Display();
 	    final Shell shell = new Shell(display);
 	    
 	   
-	    shell.setText("Canvas Example");
+	    shell.setText("PicSheepEditor");
 	    shell.setLayout(new FillLayout());
 
 	    Canvas canvas = new Canvas(shell, SWT.NONE);
@@ -55,10 +60,46 @@ int X,Y;
 	        public void handleEvent(Event event) {
 	         
 	        	   System.out.println("Down");
-	            X = event.x;
-	            Y = event.y;
+	        	   int xk,xg,yk,yg;
 	            mouse = !mouse;
 	          
+	            if(!mouse&&viereck)
+	            {
+	            	X = event.x;
+		            Y = event.y;
+	            
+	            }
+	            if(mouse&&viereck)
+	            {
+	         	   if(X<event.x)
+        		   {
+        			   xk=X;
+        			   xg=event.x;
+        		   }
+        		   else
+        		   {
+        			   xk=event.x;
+        			   xg=X;
+        		   }
+        		   if(Y<event.y)
+        		   {
+        			   yk=Y;
+        			   yg=event.y;
+        		   }
+        		   else
+        		   {
+        			   yk=event.y;
+        			   yg=Y;
+        		   }	
+        		   GC gc = new GC(shell);
+        		   gc.drawRectangle(xk, yk, xg-xk,yg-yk);
+        		   gc.dispose(); 
+	            	
+	            		
+		            
+		            
+	            }
+	            
 	        }
 	      };
 	    
@@ -69,18 +110,68 @@ int X,Y;
 	              /*if ((event.stateMask & SWT.BUTTON1) == 0)
 	                break;*/
 	        	   System.out.println("Move");
-	        	   if(mouse)
+	        	  
+	        	   if(!mouse&&bleistift)
 	        	   {
-	        	   GC gc = new GC(shell);
-	               gc.drawLine(X, Y, event.x, event.y);
-	               gc.dispose();  	    
+	        		   X = event.x;
+			            Y = event.y;
 	        	   }
-	        	   X = event.x;
+	        	   if(mouse&&bleistift)
+	        	   	{
+	        		  
+	        		   GC gc = new GC(shell);
+	        		   gc.drawLine(X, Y, event.x,event.y);
+	        		   gc.dispose(); 
+	               X = event.x;
 		            Y = event.y;
+	        	   }
+	        	   
 	          }
 	        };
 	    canvas.addListener(SWT.MouseDown, listenerDown);
 	    canvas.addListener(SWT.MouseMove, listenerMove);
+	    
+	    final Button button = new Button(shell, SWT.PUSH);
+	    button.setText("Bleistift");
+	    
+	    button.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("Bleistift");
+				bleistift = !bleistift;
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+	       
+	      });
+	    
+	    final Button buttonViereck = new Button(shell, SWT.PUSH);
+	    buttonViereck.setText("Viereck");
+	    
+	    buttonViereck.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("Bleistift");
+				viereck = !viereck;
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+	       
+	      });
 	    
 	    shell.open();
 	    while (!shell.isDisposed()) {
